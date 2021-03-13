5c0c123dea06ceb6708ad852031acfe36b83060d
Jenny Russell <moosyoolka@gmail.com>
2021-03-13 12:15:17 -0800

    latests changes

--- js/YelpApi.js
+++ js/YelpApi.js
@@ -3,30 +3,45 @@ const CLIENT_ID = "cdcxCdVgpGDur80CjemkRg";
 
 let baseURL = 'https://api.yelp.com/v3/';
 
-
 //testing location
 const latitude = 33.46945063148673
 const longitude = -117.1231696967713
 
-function getBusinessByLatLon(lat, lon) {
+
+function getBusinessByLatLon(lat, lon, callback) {
     const business = `businesses/search?latitude=${lat}&longitude=${lon}`
     let businessLatLon = `${baseURL}${business}`
-    return baseFetch(businessLatLon, myCallBack)
+    console.log("Hello");
+    return baseFetch(businessLatLon, callback)
 }
 
+function getLatLong() {
+    $(document).ready(function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+        } else {
+            alert('It seems like Geolocation, which is required for this page, is not enabled in your browser.');
+        }
+    });
 
-function successFunction(position) {
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    alert('Your latitude is :' + lat + ' and longitude is ' + long);
-}
 
-function errorFunction(position) {
-    alert('Error!');
-}
+
+    function successFunction(position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        // alert('Your latitude is :' + lat + ' and longitude is ' + long);
+        document.getElementById("long").innerText = lat;
+        document.getElementById("lat").innerText = long;
+    }
+
+    function errorFunction(position) {
+        alert('Error!');
+    }
+};
+
 
 function testApi() {
-    getBusinessByLatLon(latitude, longitude);
+    getBusinessByLatLon(latitude, longitude, myCallBack);
 }
 
 function myCallBack(json) {
@@ -37,7 +52,9 @@ testApi();
 
 
 
+
 async function baseFetch(baseURL, callback) {
+    console.log(baseURL);
     var req = new Request(baseURL, {
         method: 'GET',
         headers: new Headers({
